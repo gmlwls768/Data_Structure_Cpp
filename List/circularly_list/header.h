355c0a5e9c4b15da141806a8580f5ff0541c57aa
KIMHEEJIN <gmlwls768@naver.com>
2022-10-17 15:23:24 +0900

    List Data Structure

---
+++ List/circularly_list/header.h
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+typedef string Elem;
+class CNode
+{
+private:
+    Elem elem;
+    CNode *next;
+
+    friend class CircleList;
+};
+
+class CircleList
+{
+public:
+    CircleList();
+    ~CircleList();
+    bool empty() const;
+    const Elem &front() const;
+    const Elem &back() const;
+    void advance();
+    void add(const Elem &e);
+    void remove();
+
+private:
+    CNode *cursor;
+};
