b13c9e3a13d353059b74715d8631cfe1618a2d27
KIMHEEJIN <gmlwls768@naver.com>
2022-10-21 12:22:30 +0900

    List Data Structure

--- Stack/linkedlist_Stack/Slink.cpp
+++ Stack/linkedlist_Stack/Slink.cpp
@@ -1,25 +1,36 @@
-#include "header.h"
+#include "Slink.h"
 
 StringLinkedList::StringLinkedList() // constructor
-    :head(NULL) {}
+    : head(NULL)
+{
+}
 
 StringLinkedList::~StringLinkedList() // destructor
-    {while (!empty()) removeFront(); }
+{
+    while (!empty())
+        removeFront();
+}
 
 bool StringLinkedList::empty() const // is list empty
-    {return head == NULL;}
+{
+    return head == NULL;
+}
 
 const string StringLinkedList::front() const // get front element
-    {return head->elem;}
-
-void StringLinkedList::addFront(const string& e) { //add to front of list
-    StringNode* v = new StringNode;  //create new node
-    v->elem = e;  //store data
-    v->next = head;  //head now follows v
-    head = v;  //v is now the head
+{
+    return head->elem;
 }
-void StringLinkedList::removeFront(){ // remove front item
-    StringNode* old = head; // save current head
-    head = old->next; // skip over old head
-    delete old; //delete the old head
+
+void StringLinkedList::addFront(const string &e)
+{                                   // add to front of list
+    StringNode *v = new StringNode; // create new node
+    v->elem = e;                    // store data
+    v->next = head;                 // head now follows v
+    head = v;                       // v is now the head
+}
+void StringLinkedList::removeFront()
+{                           // remove front item
+    StringNode *old = head; // save current head
+    head = old->next;       // skip over old head
+    delete old;             // delete the old head
 }
